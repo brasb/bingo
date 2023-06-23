8f60d6ada0a2678dc0ef985946d45812e3b4ce91
gitbrasedu <127907909+gitbrasedu@users.noreply.github.com>
2023-06-23 16:54:22 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,37 @@
-/*
-    Bingo:
-        * criação de tabelas;
-        * 25 números aleatórios;
-        *   * de 1 a 75;
-        *   * sem repetições;
-        *   * B: só de 1 a 15;
-        *   * I: só de 16 a 30;
-        *   * N: só de 31 a 45;
-        *   * G: só de 46 a 60;
-        *   * O: só de 61 a 75;
-        * sortear só um número por segundo e mostrá-lo na tela;
-        * marcar cartelas durante o sorteio;
-        * deverá ser indicado se um ou mais jogadores ganharem o jogo.
-*/
-// Mínimo 24 número sorteados, intervalo 1s
-
+// As tabelas são divididas em várias linhas (divs) com o id linha_x.
 const PREFIXO_ID_CONTAINER = "linha_";
+// Cor usada para preencher uma coluna que teve o número sorteado.
+const COR_PREENCHIMENTO = "rgb(54, 255, 55)";
 
-// Sorteados é a lista de números que já foram sorteados, para evitar repetição.
-function gerarNumero(sorteados, min, max)
+// Definida automaticamente.
+let corPadrao;
+// Lista de números já sorteados para evitar repetição e mostrar na seção de resultados.
+let sorteados = [];
+// ID do set interval usado para a função de atualizar o sorteio periodicamente.
+let idSetInterval;
+// Se um sorteio está sendo feito pela primeira vez.
+let primeiraVez = true;
+let quantidadeCartelas = 0;
+
+/*
+        Gera um número inteiro pseudoaleatoriamente.
+
+    blacklist -> lista de números que não podem ser sorteados.
+    min -> valor mínimo do número sorteado (inclusivo).
+    max -> valor máximo do número sorteado (inclusivo).
+*/
+function gerarNumero(blacklist, min, max)
 {
     // Fica sorteando até encontrar um número único.
-    let n = -1;
-    while (n === -1 || sorteados.includes(n)) {
+    let n;
+    while (n === undefined || blacklist.includes(n)) {
         n = Math.ceil(Math.random() * (max - min) + min);
     }
-    
+
     return n;
 }
 
+// Garante que todos os números do bingo terão uma quantidade de dígitos uniforme.
 function formatarNumero(n)
 {
     let result = parseInt(n);
@@ -37,7 +40,20 @@ function formatarNumero(n)
 
 function adicionarCartela()
 {
-    let nome = prompt("Digite o nome do jogador:");
+    // O ID é indefinido enquanto nenhum sorteio estiver sendo realizado.
+    if (idSetInterval !== undefined) {
+        alert("Você não pode adicionar tabelas durante o sorteio!");
+        return;
+    }
+
+    let nome = "";
+    while (nome === "") {
+        nome = prompt("Digite o nome do jogador:");
+        if (nome === "") {
+            alert("Não deixe o nome vazio!");
+        }
+    }
+
     gerarCartela(nome);
 }
 
@@ -59,7 +75,8 @@ function retornarContainerTabela(divPai, classe)
     return resultado;
 }
 
-function criarContainer(divJogos, numeroId) {
+function criarContainer(divJogos, numeroId)
+{
     let novoContainer = document.createElement("div");
     novoContainer.setAttribute("class", "linha_jogo");
     novoContainer.setAttribute("id", PREFIXO_ID_CONTAINER + numeroId);
@@ -67,16 +84,16 @@ function criarContainer(divJogos, numeroId) {
     return novoContainer;
 }
 
-function obterContainerDisponivel() 
+function obterContainerDisponivel()
 {
     const JOGOS = document.getElementById("jogos");
 
     if (JOGOS.childElementCount === 0) {
         return criarContainer(JOGOS, 0);
     }
-    
+
     let container = null;
-    let i = 0;    
+    let i = 0;
     while (container === null) {
         let id = PREFIXO_ID_CONTAINER + i;
         let elemento = document.getElementById(id);
@@ -88,7 +105,7 @@ function obterContainerDisponivel()
 
         if (elemento.childElementCount < 4) {
             container = elemento;
-        } 
+        }
 
         i++;
     }
@@ -98,9 +115,10 @@ function obterContainerDisponivel()
 
 function gerarCartela(nome)
 {
+    quantidadeCartelas += 1;
 
     // A div de container é diferente, dependendo da quantidade de cartelas criadas.
-    let container = obterContainerDisponivel(); 
+    let container = obterContainerDisponivel();
 
     let jogoDiv = document.createElement("div");
     jogoDiv.setAttribute("class", "jogo");
@@ -114,7 +132,7 @@ function gerarCartela(nome)
     title.innerText = nome;
     tituloDiv.appendChild(title);
 
-    let cartelaDiv = document.createElement("cartela");
+    let cartelaDiv = document.createElement("div");
     cartelaDiv.setAttribute("class", "cartela");
     jogoDiv.appendChild(cartelaDiv);
 
@@ -170,11 +188,12 @@ function gerarCartela(nome)
     }
 }
 
-function formatarResultados(resultados)
-{
-    let texto = "";
+function formatarNumerosSorteados(resultados) {
+    let texto = "Números sorteados: ";
+    const TAMANHO_PADRAO = texto.length;
+
     for (let i = 0; i < resultados.length; i++) {
-        if (texto !== "") {
+        if (texto.length !== TAMANHO_PADRAO) {
             texto += ", ";
             let linhas = texto.split("\n");
             if (linhas[linhas.length - 1].length >= 80) {
@@ -187,35 +206,100 @@ function formatarResultados(resultados)
     return texto;
 }
 
-let sorteados = [];
+function eNumerico(texto)
+{
+    return !isNaN(parseInt(texto));
+}
+
 function atualizarSorteio()
 {
     let n = gerarNumero(sorteados, 1, 75);
     sorteados.push(n);
 
+    let vencedores = []
+    let jogos = document.getElementsByClassName("cartela");
+    for (const jogo of jogos) {
+        let tds = jogo.getElementsByTagName("td");
+
+        let bingo = true;
+        for (const td of tds) {
+            if (!eNumerico(td.innerText)) {
+                corPadrao = td.style.backgroundColor;
+                continue;
+            }
+
+            if (td.innerText == n) {
+                td.style.backgroundColor = COR_PREENCHIMENTO;
+            }
+
+            if (td.style.backgroundColor != COR_PREENCHIMENTO) {
+                bingo = false;
+            }
+        }
+
+        if (bingo) {
+            vencedores.push(jogo.parentNode.getElementsByTagName("h3")[0].innerText);
+            clearInterval(idSetInterval);
+            idSetInterval = undefined;
+        }
+    }
+
     let containerResultados = document.getElementById("container_resultados");
-    containerResultados.innerText = formatarResultados(sorteados);
+    containerResultados.innerText = formatarNumerosSorteados(sorteados);
+
+    if (vencedores.length > 0) {
+        containerResultados.innerText += "\n\nVencedores: " + vencedores.join(", ") + "."
+    }
 }
 
-function iniciarSorteio() 
+function desfazerSorteio()
 {
-    setInterval(atualizarSorteio, 200);
+    sorteados = []
+
+    let containerResultados = document.getElementById("container_resultados");
+    containerResultados.innerText = "";
+
+    let tds = document.getElementsByTagName("td");
+    for (const td of tds) {
+        if (eNumerico(td.innerText)) {
+            td.style.backgroundColor = corPadrao;
+        }
+    }
+}
+
+function iniciarSorteio()
+{
+    // O ID é indefinido enquanto nenhum sorteio estiver sendo realizado.
+    if (idSetInterval !== undefined) {
+        alert("Espere o sorteio atual terminar antes de começar outro!");
+        return;
+    }
+
+    if (quantidadeCartelas < 1) {
+        alert("Crie uma ou mais tabelas antes de começar o sorteio.");
+        return;
+    }
+
+    if (!primeiraVez) {
+        desfazerSorteio();
+    }
+
+    idSetInterval = setInterval(atualizarSorteio, 1000);
+    primeiraVez = false;
 }
 
 function limparTabelas()
 {
+    // O ID é indefinido enquanto nenhum sorteio estiver sendo realizado.
+    if (idSetInterval !== undefined) {
+        alert("Você não pode limpar as tabelas durante o sorteio!");
+        return;
+    }
+
+    quantidadeCartelas = 0;
+
     let jogos = document.getElementById("jogos");
     while (jogos.firstChild !== null) {
         jogos.firstChild.remove();
     }
 }
-
-gerarCartela("João");
-gerarCartela("Carlos");
-gerarCartela("Afonso");
-gerarCartela("Valéria");
-gerarCartela("Irineu");
-gerarCartela("Ambrósio");
-gerarCartela("Pedro");
-gerarCartela("Augustinho");
-gerarCartela("Inácio");
